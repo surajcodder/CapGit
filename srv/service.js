25055dfd90f08fec9d9e853635677d8790f81547
Suman Senapati <suman.senapati@peolsolutions.com>
2024-09-18 09:30:24 +0000

    hello

--- srv/service.js
+++ srv/service.js
@@ -4,5 +4,23 @@ module.exports = cds.service.impl(async function(srv){
         console.log(req.data.input)
         return `Hello ${req.data.input}!`
     })
+    srv.on('addition', req=>{
+        var result = req.data.num1 + req.data.num2;
+        return result;
+    })
+    srv.on('myFunction', req=>{
+        let result1 = {}
+        if(req.data.catagory == 1){
+            result.product = 'BMW',
+            result.price = '4200 USD',
+            result.location = 'LONDON'
+        } else{
+            result.product = 'AUDI',
+            result.priceArray = {
+                'price': 23345,
+                'color': 'red'
+            }
+        }
+    })
 
 })
